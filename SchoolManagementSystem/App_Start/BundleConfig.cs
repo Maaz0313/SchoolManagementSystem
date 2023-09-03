867de31d5940cd2a05418e0e37173148cfadd68f
Maaz0313 <maaztajammul63@gmail.com>
2023-09-03 18:55:06 +0500

    added jQuery files to BundleConfig.cs

--- SchoolManagementSystem/App_Start/BundleConfig.cs
+++ SchoolManagementSystem/App_Start/BundleConfig.cs
@@ -9,15 +9,19 @@ namespace SchoolManagementSystem
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery.*"));
+                        "~/Scripts/jquery-3.4.1.js",
+                        "~/Scripts/jquery-3.4.1.slim.js",
+                        "~/Scripts/jquery-3.4.1.intellisense.js",
+                        "~/Scripts/jquery-ui-timepicker-addon.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js"));
 
             //// Use the development version of Modernizr to develop with and learn from. Then, when you're
             //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
